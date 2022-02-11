bdee5025b08ee3be86fe35dca4fe3cf8c267109c
Zalexanninev15 <blue.shark@disroot.org>
2022-02-11 14:23:18 +0300

    VitNX v2.4.4.2 [DEV2.4.2]
    Changes:
    - Fixed all errors with code S2223 (more details: https://rules.sonarsource.com/csharp/RSPEC-2223)
    - Ended the transition to a more secure way of obtaining hardware data

--- source/VitNX/Properties/AssemblyInfo.cs
+++ source/VitNX/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("f19472f5-8c44-4c51-a8a0-b9de5f555255")]
-[assembly: AssemblyVersion("2.4.4.2")]
-[assembly: AssemblyFileVersion("2.4.4.2")]
+[assembly: AssemblyVersion("2.4.4.3")]
+[assembly: AssemblyFileVersion("2.4.4.3")]
