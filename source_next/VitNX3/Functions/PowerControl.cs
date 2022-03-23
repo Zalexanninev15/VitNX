c8a5ab573f3c6b4eb1c90f3f071454fa8c2a45b1
Zalexanninev15 <blue.shark@disroot.org>
2022-03-23 12:26:34 +0300

    🎈 VitNX3 for .NET 5
    First alpha version for .NET 5! Frankly speaking, this is my first time migrating a project, and I use .NET 5+ not too often, but nevertheless. Version 3-alpha
    
    I had to cut something out because it won't work as well on .NET anymore, so the functionality has changed. Also haven't moved the Controls yet, will do so later.

---
+++ source_next/VitNX3/Functions/PowerControl.cs
@@ -0,0 +1,55 @@
+﻿using VitNX.Functions.AppsAndProcesses;
+using VitNX.Functions.Win32;
+
+namespace VitNX.Functions
+{
+    /// <summary>
+    /// Work with the power control.
+    /// </summary>
+    public class PowerControl
+    {
+        /// <summary>
+        /// Options for control the power of computer.
+        /// </summary>
+        public enum SYSTEM_POWER_CONTROL
+        {
+            SYSTEM_LOGOFF,
+            SYSTEM_SHUTDOWN,
+            SYSTEM_REBOOT,
+            SYSTEM_LOCK
+        }
+
+        /// <summary>
+        /// The power of computer.
+        /// </summary>
+        /// <param name="method">The method.</param>
+        public static void Computer(SYSTEM_POWER_CONTROL method)
+        {
+            switch (method)
+            {
+                case SYSTEM_POWER_CONTROL.SYSTEM_LOGOFF:
+                    {
+                        Import.ExitWindowsEx(0, 0);
+                        break;
+                    }
+                case SYSTEM_POWER_CONTROL.SYSTEM_SHUTDOWN:
+                    {
+                        // Old code: Processes.Run("shutdown", "/s /t 0");
+                        Processes.Run("powershell", "Stop-Computer");
+                        break;
+                    }
+                case SYSTEM_POWER_CONTROL.SYSTEM_REBOOT:
+                    {
+                        // Old code: Processes.Run("shutdown", "/r /t 0");
+                        Processes.Run("powershell", "Restart-Computer -Force");
+                        break;
+                    }
+                case SYSTEM_POWER_CONTROL.SYSTEM_LOCK:
+                    {
+                        Import.LockWorkStation();
+                        break;
+                    }
+            }
+        }
+    }
+}
