1f14bb246af65d2ef1cacbc48afb63f3956997bc
Zalexanninev15 <blue.shark@disroot.org>
2022-03-25 12:23:07 +0300

    VitNX v3 [Alpha-2.0.0.1] and VitNX v2.9.2b-1
    Changes:
    - Improved the Windows 11 window rounding feature, which now always works perfectly
    - Removed superfluous code

--- source/VitNX/Functions/WindowAndControls.cs
+++ source/VitNX/Functions/WindowAndControls.cs
@@ -86,29 +86,17 @@ namespace VitNX.Functions.WindowAndControls
         /// Applying Windows 11 roundings to a window(s).
         /// </summary>
         /// <param name="Handler">The handler.</param>
-        public static void SetWindowsElevenStyleForWinForm(IntPtr Handler)
+        /// <param name="windowWidth">The width of window</param>
+        /// <param name="windowHeight">The height of window</param>
+        public static Region SetWindowsElevenStyleForWinForm(IntPtr Handler, int windowWidth, int windowHeight)
         {
             var attribute = Constants.DWMWA_WINDOW_CORNER_PREFERENCE;
             var preference = DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_ROUND;
             Import.DwmSetWindowAttribute(Handler,
                 attribute,
                 new[] { Convert.ToInt32(preference) }, sizeof(uint));
+            return Region.FromHrgn(Import.CreateRoundRectRgn(0, 0, windowWidth, windowHeight, 15, 15));
         }
-
-        //public static async Task WindowNormalStartAnimationAsync(double Opacity)
-        //{
-        //    for (Opacity = 0; Opacity < 1; Opacity += 0.05)
-        //        await Task.Delay(10);
-        //}
-
-        //public static void WindowNormalExitAnimation(double Opacity)
-        //{
-        //    for (int i = 0; i < 10; i++)
-        //    {
-        //        Thread.Sleep(20);
-        //        Opacity = Opacity - 0.05;
-        //    }
-        //}
     }
 
     public class Controls
