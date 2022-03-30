b7eabbfbc14914e251f8d323ce1a873ea14af2f9
Zalexanninev15 <blue.shark@disroot.org>
2022-03-30 19:02:49 +0300

    Added 3 new functions for WinAPI
    - NtQueryTimerResolution
    - NtSetTimerResolution
    - NtDelayExecution (unsafe)

--- source/VitNX3/Functions/Win32.cs
+++ source/VitNX3/Functions/Win32.cs
@@ -716,6 +716,20 @@ namespace VitNX3.Functions.Win32
             int cY,
             uint uFlags);
 
+        [DllImport("ntdll.dll", SetLastError = true)]
+        static extern int NtQueryTimerResolution(out int MinimumResolution,
+        out int MaximumResolution,
+        out int CurrentResolution);
+
+        [DllImport("ntdll.dll", SetLastError = true)]
+        static extern int NtSetTimerResolution(int DesiredResolution,
+            bool SetResolution,
+            out int CurrentResolution);
+
+        [DllImport("ntdll.dll", SetLastError = true)]
+        static unsafe extern int NtDelayExecution(bool alertable,
+            long* delayInterval);
+
         [DllImport("user32.dll")]
         public static extern int SetWindowRgn(IntPtr hWnd,
             IntPtr hRgn,
